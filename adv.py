662feffedc2a04eee41a0e46a0b5f751dd758d1c
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-05-14 20:32:03 -0600

    mvp solution

--- adv.py
+++ adv.py
@@ -12,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-map_file = "maps/test_cross.txt"
+# map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-# map_file = "maps/main_maze.txt"
+map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,6 +29,7 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+Goal_traversal_path = ['n', 'n', 's', 's', 'e', 'e', 'w', 'w', 's', 's', 'n', 'n', 'w', 'w']
 
 # ---------- Graph Setup ---------#
 from util import Queue, Stack
@@ -38,6 +39,12 @@ class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
     def __init__(self):
         self.vertices = {}
+        self.opposites = {
+            'n': 's',
+            'e': 'w',
+            's': 'n',
+            'w': 'e'
+        }
 
     def add_vertex(self, vertex_id):
         """
@@ -66,46 +73,43 @@ class Graph:
             neighbor = current_room.get_room_in_direction(direction)
             neighbors.append(neighbor.id)
         return neighbors
-    def dft(self, starting_vertex):
+    def dft(self, current_room):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        stack = Stack()
-        stack.push(starting_vertex)
-        visited = set()
-        while stack.size() > 0:
-            current_vertex = stack.pop()
-            # print(current_vertex)
-            if current_vertex not in visited:
-                visited.add(current_vertex)
-                # print(current_vertex)
-                for next_vertex in self.get_neighbors(current_vertex):
-                    stack.push(next_vertex)
-        return visited
-        # for vertex in visited:
-        #     print(vertex)
+        '''
+        variables: previous, current, direction
+        '''
+        possibility_path = []
+        def search(current_room, visited=None, last_direction_traveled=None):
+            nonlocal possibility_path
+            exits = current_room.get_exits()
+            if visited is None:
+                visited = set()
+            visited.add(current_room)
+            for direction in exits:
+                next_room = current_room.get_room_in_direction(direction)
+                if next_room not in visited:
+                    possibility_path.append(direction)
+                    search(next_room, visited, direction)
+                    # traversal_path.append(possibility_path)
+                    # for item in possibility_path:
+                    #     traversal_path.append(item)
+            if last_direction_traveled is not None:
+                possibility_path.append(self.opposites[last_direction_traveled])
+        # -----Currently doesn't account for forks of forks ------ #
+                    # return_trip = reversed(possibility_path)
+                    # for item in return_trip:
+                    #     traversal_path.append(self.opposites[item])
 
-    def dfs(self, starting_vertex, destination_vertex):
-        """
-        Return a list containing a path from
-        starting_vertex to destination_vertex in
-        depth-first order.
-        """
-        stack = Stack()
-        stack.push(starting_vertex)
-        visited = set()
-        path = []
-        while stack.size() > 0:
-            current_vertex = stack.pop()
-            if current_vertex == destination_vertex:
-                path.append(current_vertex)
-                return path
-            if current_vertex not in visited:
-                visited.add(current_vertex)
-                path.append(current_vertex)
-                for next_vertex in self.get_neighbors(current_vertex):
-                    stack.push(next_vertex)
+            # possibility_path = []
+                    # return_trip = []
+
+        search(current_room)
+        return possibility_path
+
+
 
 # ---------- Graph Setup ---------#
 
@@ -118,7 +122,8 @@ for room in world.rooms:
     # print(f"{world.rooms[room].name} : {graph.vertices[world.rooms[room].id]}")
 # print(world.rooms[0].get_exits())
 # print(graph.get_neighbors(player.current_room.id))
-print(graph.dft(player.current_room.id))
+traversal_path = graph.dft(player.current_room)
+# print(f"Traversal Path: {traversal_path}")
 # ---------- Map Traversal ---------#
 
 
